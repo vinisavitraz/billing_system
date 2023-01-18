86d902bbb9e20deae73f80159c7d79112080584e
Vinicius Savitraz <viniciusemds@gmail.com>
2023-01-18 00:18:05 -0300

    introduce local files interceptor

--- src/billing/billing.controller.ts
+++ src/billing/billing.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import LocalFilesInterceptor from 'src/app/api/local-files.interceptor';
 import { BillingService } from './billing.service';
 
 @Controller('billing')
 export class BillingController {
   constructor(private readonly billingService: BillingService) {}
+
+  @Post()
+  @UseInterceptors(LocalFilesInterceptor({
+    fieldName: 'billings',
+    path: '/csv'
+  }))
+  public async saveBillingsFile(@UploadedFile() fileCSV: Express.Multer.File): Promise<void> {    
+    console.log(fileCSV.originalname);
+  }
 }
